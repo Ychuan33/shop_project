5376e6ee49836680b40e196af04d2ea4c00a415c
Ychuan33 <felixyan2011@gmail.com>
2016-07-21 11:52:15 -0400

    shop.catrl

--- shop.ctrl.js
+++ shop.ctrl.js
@@ -3,11 +3,20 @@
 		.module('shopApp')
 		.controller('ShopCtrl',ShopCtrl)
 
-	function ShopCtrl($scope,productSrv,products, cartSrv){
+	function ShopCtrl($scope,productSrv,products,cartSrv){
 		var shopVm = this;
 
 		//TODO #3 Capture resolved products for view
-		shopVm.products = products;
+		shopVm.products=productSrv.products;
+
+		// productSrv.getProducts()
+		// .then(function(data){
+		// 	console.log(data)
+		// })
+		// .catch(function(err){
+		// 	console.log(err)
+			
+		// })
 
 		//watch for any changes to model data
 		$scope.$watch(function(){
@@ -16,20 +25,36 @@
 		    shopVm.products = productSrv.products;
 		});
 
+		// $scope.$watch(
+		// 	function(){
+		// 		return productSrv.products;
+		// 	}
+
+		// 	function(newVal){
+				
+		// 	}
+
+		// 	)
+
 		shopVm.openCart = function(){
 			cartSrv.open();
 		}
+
+		shopVm.addCart = function(id){
+			//run getProduct function from productSrv
+
+			//take response and add to cart array in cartSrv
+
+								
+
+
+		}
+
+		
+
+
+
 	}
-
-
-
-	// $scope.$watch ()
-// what youre watching
-// function ( {
-	// return productsSrv.products;
-// },
-// what to do when it changes
-// function (newValue) {  (route forcing link)
 
 })();
 
