24d72e1645c71d1012128bed30173685086bf328
haamna <haamna.ehsan@gmail.com>
2016-07-21 18:48:45 -0400

    updated checkout

--- app/site/controllers/checkout.ctrl.js
+++ app/site/controllers/checkout.ctrl.js
@@ -1,15 +1,29 @@
 (function(){
 	angular
 		.module('shopApp')
-		.controller('CheckoutCtrl',CheckoutCtrl);
-
-function CheckoutCtrl($scope,$state,items,cartSrv){
-
-	var checkVm=this;
+		.controller('CheckoutCtrl',CheckoutCtrl)
 
 
+	function CheckoutCtrl($scope,$state,productSrv,cartSrv){
+		var checkVm = this;
+
+		checkVm.items = cartSrv.items;
+		console.log (checkVm.items);
+		checkVm.getTotal = getTotal;
+		checkVm.Total = checkVm.getTotal() 
+
+	 	function getTotal() {
+	 		var totalCost = 0;
+	 		for  (var i=0; i<checkVm.items.length; i++){
+	 			totalCost += checkVm.items[i].price * checkVm.items[i].quantity
+			
+	 		}
+	 		return totalCost;
+	 	
+		}
+	}
 
 
-
-}
 })();
+
+
