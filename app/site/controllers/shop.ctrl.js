aa94d9091615a51a158191a282da61f0fc663982
Ychuan33 <felixyan2011@gmail.com>
2016-07-21 13:45:35 -0400

    new

---
+++ app/site/controllers/shop.ctrl.js
@@ -0,0 +1,68 @@
+(function(){
+	angular
+		.module('shopApp')
+		.controller('ShopCtrl',ShopCtrl)
+
+	function ShopCtrl($scope,productSrv,products,cartSrv){
+		var shopVm = this;
+
+		//TODO #3 Capture resolved products for view
+		shopVm.products=productSrv.products;
+
+		// productSrv.getProducts()
+		// .then(function(data){
+		// 	console.log(data)
+		// })
+		// .catch(function(err){
+		// 	console.log(err)
+			
+		// })
+
+		//watch for any changes to model data
+		$scope.$watch(function(){
+	    	return productSrv.products;
+		}, function (newValue) {
+		    shopVm.products = productSrv.products;
+		});
+
+		// $scope.$watch(
+		// 	function(){
+		// 		return productSrv.products;
+		// 	}
+
+		// 	function(newVal){
+				
+		// 	}
+
+		// 	)
+
+		
+
+		shopVm.openCart = function(){
+			cartSrv.open();
+		}
+
+		shopVm.addCart = function(id){
+			//run getProduct function from productSrv
+
+			//take response and add to cart array in cartSrv
+		shopVm.item = productSrv.getProduct(id);
+		console.log(id);
+		console.log(shopVm.item);
+		cartSrv.addProduct(shopVm.item);
+
+
+								
+
+
+		}
+
+		
+
+
+
+	}
+
+})();
+
+
---
+++ app/site/services/cart.srv.js
@@ -0,0 +1,62 @@
+(function(){
+    
+    angular
+        .module('shopApp')
+        .service('cartSrv', cartService)
+
+        function cartService($uibModal){
+        	var self = this;
+
+        	self.items = [];
+            self.addProduct=addProduct;
+
+        	function addProduct(product){
+        		self.items.push(product)
+        	}
+
+        	//Display Items In Cart
+        	// cart controller:
+        		//ctrl.cart = cartSrv.items
+        		//connect controller cart to cart service
+
+        	// Cart.HTML
+        	// ng-repeat = "item in ctrl.cart"
+        		//display controller cart in html
+
+
+        	
+        	//Adding Product to Cart
+        	//main product page HTML
+        	//ng-repeat= "item in ctrl.products"
+        			// display each item on the page
+
+        	//ng-click= "ctrl.addProduct(item)"
+        			// when button clicked, add that specific item to cart
+
+        	// Must be repeated for both Main Shop Page / Shop Controller
+        	// and Details Page // Details Controller
+
+
+        	self.open = function (size) {
+        		console.log('Opening modal');
+        		var modalInstance = $uibModal.open({
+			      animation: false,
+			      templateUrl: 'site/partials/cart.html',
+			      controller: 'ModalInstanceCtrl as ctrl',
+			      size: size,
+			      resolve: {
+			        items: function () {
+			          return self.items;
+			        }
+		      }
+		    });
+
+		    modalInstance.result.then(function () {
+		      console.log('clicked okay');
+		    }, function () {
+		      console.log('Modal dismissed at: ' + new Date());
+		    });
+		  };
+        }
+})();
+		
